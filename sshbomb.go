6275905f7c039f39aa21e4f599a0885493f72756
Eric Lesh <eclesh@gmail.com>
2014-01-04 16:04:38 -0800

    Initial commit

---
+++ sshbomb.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	listenFlag = flag.String("listen", ":2020", "address to listen on")
+	sizeFlag   = flag.Int("size", 1024*1024, "size in bytes of data to send")
+)
+
+func handle(c net.Conn) {
+	defer c.Close()
+
+	log.Println("connection from", c.RemoteAddr())
+
+	c.SetDeadline(time.Now().Add(10 * time.Second))
+
+	f, err := os.Open("/dev/urandom")
+	if err != nil {
+		log.Println("can't open:", err)
+		return
+	}
+
+	data := make([]byte, *sizeFlag)
+	rsize, err := f.Read(data)
+	if err != nil {
+		log.Println("can't read:", err)
+		return
+	}
+
+	wsize, err := c.Write(data)
+	if err != nil {
+		log.Printf("write error (%d of %d bytes were written)", wsize, rsize)
+		return
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	log.Println("*** listening on", *listenFlag)
+
+	l, err := net.Listen("tcp", *listenFlag)
+	if err != nil {
+		log.Fatalln("can't listen:", err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("can't accept:", err)
+			continue
+		}
+
+		go handle(conn)
+	}
+}
